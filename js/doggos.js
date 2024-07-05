daab2a74767ee213f07e18320f1090993e4fb95d
TK <118142360+tak40@users.noreply.github.com>
2024-07-05 10:26:20 -0400

    Enhance functionality: Shift focus to breed-specific dog image generation
    - Transitioned from fetching random dog images to a breed-specific image generation approach.
    - Updated the JavaScript code to fetch a list of all dog breeds and populate a dropdown menu with breed options.
    - Implemented a 'change' event listener on the dropdown to fetch and display a random image of the selected breed.
    - Created a new function 'getDoggo' to handle the API call for fetching breed-specific images and display them dynamically.
    - Improved the user interface by making it more interactive and user-focused.
    - Removed previous implementation for fetching random dog images, reflecting the new direction towards breed-specific functionality.

--- js/doggos.js
+++ js/doggos.js
@@ -1,4 +1,4 @@
-//  Dog API
+/* //  Dog API
 //  https://dog.ceo/dog-api/
 
 // 1. Fetch the random image of a dog
@@ -20,3 +20,40 @@ function addDoggo() {
 }
 
 document.querySelector(".add-doggo").addEventListener("click", addDoggo);
+ */
+
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+const select = document.querySelector(".breeds");
+
+fetch(BREEDS_URL) // fetch returns a promise
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    const breedsObject = data.message;
+    console.log(breedsObject);
+    const breedsArray = Object.keys(breedsObject);
+    console.log(breedsArray);
+
+    for (let i = 0; i < breedsArray.length; i++) {
+      const option = document.createElement("option");
+      option.value = breedsArray[i];
+      option.innerText = breedsArray[i];
+      select.appendChild(option);
+    }
+  });
+
+select.addEventListener("change", function (event) {
+  const url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+  getDoggo(url);
+});
+
+
+getDoggo(url) {
+    // make URL
+    // show loading spinner
+    // fetch from the API
+    // use the URL to change the current image
+    // hide the loading spinner
+}
